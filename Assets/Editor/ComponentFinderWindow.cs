6668687ee63be79ada64a6c01050694409524033
UQ Times <uqtimes@gmail.com>
2015-03-15 10:27:24 +0900

    Add component finder.

---
+++ Assets/Editor/ComponentFinderWindow.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace ComponentFinder
+{
+    public class ComponentFinderWindow : EditorWindow
+    {
+        string userKeyword = string.Empty;
+        Vector2 scrollPosition;
+        List<Component> resultComponents = new List<Component>();
+
+        [MenuItem("Window/Component Finder")]
+        static void Open()
+        {
+            ComponentFinderWindow.GetWindow<ComponentFinderWindow>("CompFinder");
+        }
+
+        void OnGUI()
+        {
+            // Check user input
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Keyword: ", GUILayout.Width(58));
+            userKeyword = EditorGUILayout.TextField(userKeyword, GUILayout.Width(Screen.width - 70));
+            EditorGUILayout.EndHorizontal();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                resultComponents.Clear();
+                FindComponent(userKeyword.ToUpper());
+            }
+
+            // Show results
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+            EditorGUILayout.BeginVertical();
+            foreach (var component in resultComponents)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.ObjectField(component, typeof(Component), false);
+                EditorGUILayout.LabelField(component.GetType().ToString());
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.EndScrollView();
+        }
+
+        GameObject[] GetRootGameObjects()
+        {
+            return System.Array.FindAll(GameObject.FindObjectsOfType<GameObject>(), (item) => item.transform.parent == null);
+        }
+
+        void FindComponent(string keyword)
+        {
+            foreach (var go in GetRootGameObjects())
+            {
+                FindComponent(go, keyword);
+            }
+        }
+
+        void FindComponent(GameObject go, string keyword)
+        {
+            // Find component
+            foreach (var component in go.GetComponents(typeof(Component)))
+            {
+                var componentName = component.GetType().ToString().ToUpper();
+                if (0 < componentName.ToUpper().IndexOf(keyword))
+                {
+                    resultComponents.Add(component);
+                }
+            }
+            // Find child component
+            for (int i = 0, count = go.transform.childCount; i < count; ++i)
+            {
+                FindComponent(go.transform.GetChild(i).gameObject, keyword);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ComponentFinder
-Component finder for Unity
+# Component Finder for Unity
+
+Open [Window] > [Component Finder]
+
+入力されたコンポーネントの名前をヒエラルキーから検索します。
+
+Incremental search for typed component name from the Hierarchy.
+
+![image](https://raw.githubusercontent.com/wiki/uqtimes/ComponentFinder/images/demo.gif)
+
+Support for Unity 4 and Unity 5.
